8cbe60ced3c017425b31823612f6c9244dbf8fb5
Shivam Sah <sahshivam@outlook.com>
2021-04-24 17:32:30 +0530

    /strains page seems to be working now

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask_mysqldb import MySQL
 
 app =  Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '192.168.1.128'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD']= ''
-app.config['MYSQL_DB']='cannabis' 
+app.config['MYSQL_PASSWORD']= 'Admin@123'
+app.config['MYSQL_DB']='cannabis'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 
@@ -18,24 +18,14 @@ app.static_folder = 'static'
 
 @app.route('/strains', methods=["GET","POST"])
 def strains():
+    render_template("strains.html")
     if request.method=="POST":
         search = request.form.get("search")
-        print(search)
-    cur = mysql.connection.cursor()
-    results= cur.execute("SELECT * FROM cannabisreco WHERE Rating = :search", {"search":search}).fetchall()
-
-    #cur = mysql.connection.cursor()
-    #cur.execute("SELECT * FROM cannabisreco")
-    #results = cur.fetchall()   
-    #print(results)
-    #print(type(results))
-    #global user_search 
-    #user_search = search
-    #results= cur.execute("SELECT * FROM cannabisreco WHERE Rating = :user_search", {"user_search":user_search}).fetchall()
-        #results=cur.fetchall()
-        
-    #print(search)
-    #
+        cur = mysql.connection.cursor()
+        sqlquery = ("SELECT * from cannabis WHERE Rating = {}".format(search))
+        cur.execute(sqlquery)
+        results= cur.fetchall()
+        print(results)
     #return render_template("strains.html", data= results , ss =search)
     return render_template("strains.html",data = results)
 @app.route('/')
@@ -44,4 +34,3 @@ def age_confirmation():
 
 if __name__ =="__main__":
     app.run(debug=True)
-
